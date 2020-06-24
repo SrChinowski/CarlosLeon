dd0fc929d1a5f53cd053aaf799c1992713e63aa5
Carlos Leon <cleonve@icloud.com>
2020-06-24 00:35:12 -0500

    1st part of skeleton

--- Js/index.js
+++ Js/index.js
@@ -1,3 +1,23 @@
-$('#cv').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus')
-  })
+$(document).ready(function() 
+{
+      $('.scroll-link').on('click', function(event){
+          event.preventDefault();
+          var sectionID = $(this).attr("data-id");
+          scrollToID('#' + sectionID, 750);
+      });
+  });
+
+  function scrollToID(id, speed){
+    var offSet = 0;
+    var targetOffset = $(id).offset().top - offSet;
+    var mainNav = $('#main-nav');
+    $('html,body').animate({scrollTop:targetOffset}, speed);
+    if (mainNav.hasClass("open")) {
+        mainNav.css("height", "1px").removeClass("in").addClass("collapse");
+        mainNav.removeClass("open");
+    }
+}
+
+function showGlobe(){
+  $("#globe").is(":hidden") ? $("#globe").fadeIn() : $("#globe").fadeOut();
+}
